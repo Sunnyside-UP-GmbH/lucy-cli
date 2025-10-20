2a8d24733a5c88e3479a1af1c4818de45a2eee9d
Gradlon von Känel <gradlon@integral-systems.ch>
2025-10-20 20:50:47 +0200

    add lib to template

--- dist/commands/checks.d.ts
+++ dist/commands/checks.d.ts
@@ -4,4 +4,4 @@ import { Config } from "../config.js";
 import { AppError } from "../error.js";
 export declare const isDirectoryClean: (excludes?: string[]) => Effect.Effect<boolean, import("@effect/platform/Error").PlatformError, Config | FileSystem.FileSystem>;
 export declare const checkForDirty: (excludes?: string[]) => Effect.Effect<void, AppError | import("@effect/platform/Error").PlatformError, Config | FileSystem.FileSystem>;
-export declare const checkForVelo: () => Effect.Effect<undefined, AppError | import("effect/ParseResult").ParseError | import("@effect/platform/Error").PlatformError, Config | FileSystem.FileSystem | Path.Path>;
+export declare const checkForVelo: () => Effect.Effect<undefined, AppError | import("@effect/platform/Error").PlatformError | import("effect/ParseResult").ParseError, Config | FileSystem.FileSystem | Path.Path>;
