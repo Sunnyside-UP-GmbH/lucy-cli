7d0cceb2199eec33cd799480a336768588b03bfa
Gradlon von Känel <gradlon@integral-systems.ch>
2025-10-22 18:48:14 +0200

    removed todo's

--- src/init/index.ts
+++ src/init/index.ts
@@ -14,8 +14,6 @@ import { init_submodules } from "./gitModules.js";
 import { init_tauri } from "./tauri.js";
 import { pkgManagers } from "../schemas/lucy.js";
 
-//TODO: Update Local tsconfig for libs
-//TODO: Fix PNPM in VELO
 export const init = Effect.gen(function* (_) {
     const config = yield* Config;
     if(config.config.action.initType === undefined) {
